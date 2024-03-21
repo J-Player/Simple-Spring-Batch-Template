4ee988195720b00da69fb4d09528f34ca792abfe
João Pedro R. Diniz <jp_rd@hotmail.com>
2024-03-21 17:59:26 -0300

    Arquivos de configuração foram corrigidos e aprimorados.

--- src/main/java/com/example/configs/SpringBatchConfig.java
+++ src/main/java/com/example/configs/SpringBatchConfig.java
@@ -3,72 +3,60 @@ package com.example.configs;
 import com.example.entities.User;
 import com.example.processors.UserProcessor;
 import com.example.repositories.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.LineMapper;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+
+import java.time.Instant;
+import java.util.Collections;
 
 @Configuration
-@EnableBatchProcessing
 @RequiredArgsConstructor
 public class SpringBatchConfig {
 
-    private final UserRepository userRepository;
-
     @Bean
-    public Job job(JobRepository jobRepository, Step step) {
-        return new JobBuilder("job", jobRepository)
+    public Job job(JobRepository jobRepository, Step step, JobCompletionListener jobCompletionListener) {
+        return new JobBuilder(SpringBatchConstraints.JOB_NAME, jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(jobCompletionListener)
                 .start(step)
                 .build();
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("csv-step", jobRepository)
-                .<User, User>chunk(10, transactionManager)
+    public Step step(JobRepository jobRepository, JpaTransactionManager transactionManager, ItemWriter<User> writer) {
+        return new StepBuilder(SpringBatchConstraints.STEP_NAME, jobRepository)
+                .<User, User>chunk(200, transactionManager)
                 .reader(reader())
                 .processor(processor())
-                .writer(writer())
+                .writer(writer)
                 .build();
     }
 
     @Bean
     public FlatFileItemReader<User> reader() {
-        FlatFileItemReader<User> reader = new FlatFileItemReader<>();
-        reader.setResource(new FileSystemResource("src/main/resources/data.csv"));
-        reader.setName("csvReader");
-        reader.setLinesToSkip(1);
-        reader.setLineMapper(lineMapper());
-        return reader;
-    }
-
-    @Bean
-    public LineMapper<User> lineMapper() {
-        DefaultLineMapper<User> lineMapper = new DefaultLineMapper<>();
-        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
-        delimitedLineTokenizer.setDelimiter(",");
-        delimitedLineTokenizer.setNames("id", "username", "password", "email", "nationality", "birthDate");
-        delimitedLineTokenizer.setStrict(false);
-        BeanWrapperFieldSetMapperCustom<User> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapperCustom<>();
-        beanWrapperFieldSetMapper.setTargetType(User.class);
-        lineMapper.setLineTokenizer(delimitedLineTokenizer);
-        lineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
-        return lineMapper;
+        return new FlatFileItemReaderBuilder<User>()
+                .name("csvReader")
+                .resource(new ClassPathResource("data.csv"))
+                .delimited()
+                .names(SpringBatchConstraints.ITEM_READER_NAMES)
+                .linesToSkip(1)
+                .customEditors(Collections.singletonMap(Instant.class, new InstantPropertyEditor()))
+                .strict(true)
+                .targetType(User.class)
+                .build();
     }
 
     @Bean
@@ -77,7 +65,7 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public ItemWriter<User> writer() {
+    public ItemWriter<User> writer(UserRepository userRepository) {
         return new RepositoryItemWriterBuilder<User>()
                 .repository(userRepository)
                 .methodName("save")
